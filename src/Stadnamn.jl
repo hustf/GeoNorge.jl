d9c395a03456abacc87d9f86854a18db3cb1ce68
hustf <hustf@users.noreply.github.com>
2024-10-28 13:41:38 +0100

    modified:   Project.toml 0.1.3 modified:   src/Stadnamn.jl Move const ahead of include

--- src/Stadnamn.jl
+++ src/Stadnamn.jl
@@ -4,9 +4,9 @@ using JSON3
 import DelimitedFiles
 using DelimitedFiles: readdlm
 export get_stadnamn_url, get_stadnamn_data, explore, point_names
+const LOCAL_FNAM = "Stadnamn.csv"
+const BASE_URL = "https://api.kartverket.no/stedsnavn/v1"
 include("request.jl")
 include("point_names.jl")
 include("utils.jl")
-const LOCAL_FNAM = "Stadnamn.csv"
-const BASE_URL = "https://api.kartverket.no/stedsnavn/v1"
 end
